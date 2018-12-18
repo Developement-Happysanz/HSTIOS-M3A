c2d0f583bc716697c248b8a18ee568229968c589
HappySanzTech <admin@palprotech.com>
2018-12-18 11:44:52 +0530

    Initial Commit

---
+++ M3 Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  M3 Admin
+//
+//  Created by Happy Sanz Tech on 18/12/18.
+//  Copyright © 2018 Happy Sanz Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
